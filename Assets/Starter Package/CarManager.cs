e9e5633e1fc2ba8919e88a29245e37910a0f57d3
Hainzel <hainzelk@gmail.com>
2025-11-01 21:15:05 +0800

    Initial commit

---
+++ Assets/Starter Package/CarManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
+
+public class CarManager : MonoBehaviour
+{
+    public GameObject CarPrefab;
+    public ReticleBehaviour Reticle;
+    public DrivingSurfaceManager DrivingSurfaceManager;
+
+    public CarBehaviour Car;
+
+    private void Update()
+    {
+        if (Car == null && WasTapped() && Reticle.CurrentPlane != null)
+        {
+            // Spawn our car at the reticle location.
+            var obj = GameObject.Instantiate(CarPrefab);
+            Car = obj.GetComponent<CarBehaviour>();
+            Car.Reticle = Reticle;
+            Car.transform.position = Reticle.transform.position;
+            DrivingSurfaceManager.LockPlane(Reticle.CurrentPlane);
+        }
+    }
+
+    private bool WasTapped()
+    {
+        if (Mouse.current != null && Mouse.current.leftButton.isPressed)
+        {
+            return true;
+        }
+
+        if (Touchscreen.current == null || Touchscreen.current.touches.Count == 0)
+        {
+            return false;
+        }
+
+        var touch = Touchscreen.current.touches[0];
+        if (touch.phase.ReadValue() != UnityEngine.InputSystem.TouchPhase.Began)
+        {
+            return false;
+        }
+
+        return true;
+    }
+} 
